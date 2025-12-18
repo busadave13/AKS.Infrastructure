266aefd59b83b77232d1dd30e197266951cf58b3
Dave Harding <daveharding@outlook.com>
2025-12-17 21:29:03 -0800

    test

--- terraform/environments/dev/import.tf
+++ terraform/environments/dev/import.tf
@@ -1,4 +1,4 @@
 import {
-  to = aks.azurerm_kubernetes_cluster_node_pool.workload
+  to = module.aks.azurerm_kubernetes_cluster_node_pool.workload
   id = "/subscriptions/79a9ae56-fcc2-4717-a184-a409adf7d669/resourceGroups/rg-aks-platform-dev-wus2/providers/Microsoft.ContainerService/managedClusters/aks-platform-dev-wus2/agentPools/workload"
 }
